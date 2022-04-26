13e1b57d38afebfcfc8cc3d455d0b2b73f33264a
Armando del Río <adrio.coder@gmail.com>
2022-04-26 10:41:42 -0500

    Add basic code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [cList, setCList] = useState([]);
+
+  const submitData = (e, data, setData) => {
+    e.preventDefault();
+    const newArr = [...cList, data].sort((a, b) =>
+      a.firstName.localeCompare(b.firstName)
+    );
+    setCList(newArr);
+    setData({ firstName: '', lastName: '', phone: '' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>Save a contact to List</h3>
+      <InputForm submitData={submitData} />
+      <List cList={cList} />
     </div>
+  );
+}
+
+function InputForm({ submitData }) {
+  const [data, setData] = useState({
+    firstName: 'Coder',
+    lastName: 'Byte',
+    phone: '888558345',
+  });
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <form onSubmit={(e) => submitData(e, data, setData)}>
+      <label>
+        Name:
+        <input
+          type='text'
+          name='firstName'
+          value={data.firstName}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        lastName:
+        <input
+          type='text'
+          name='lastName'
+          value={data.lastName}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Phone:
+        <input
+          type='phone'
+          name='phone'
+          value={data.phone}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
+  );
+}
+
+function List({ cList }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Last Name</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cList.map((contact) => (
+          <tr key={uuidv4()}>
+            <td>{contact.firstName}</td>
+            <td>{contact.lastName}</td>
+            <td>{contact.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
